1399103fbdeef6b42517d4144950705730b2f0bc
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-10-03 11:56:15 +0300

    test

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,37 +1,41 @@
 <template>
-    <div class="loader">
-      <img src="/imgs/logo.jpeg" class="spinner" alt="logo" />
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  .loader {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(255, 255, 255, 0.9); /* خلفية شفافة */
-    z-index: 9999;
+  <div class="loader">
+    <img src="/imgs/logo.jpeg" class="spinner" alt="logo" />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.9);
+  /* خلفية شفافة */
+  z-index: 9999;
+}
+
+.dark .loader {
+  background-color: black
+}
+
+.spinner {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
   }
-  
-  .spinner {
-    width: 120px;
-    height: 120px;
-    border-radius: 50%;
-    animation: spin 2s linear infinite;
-  }
-  
-  @keyframes spin {
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-  </style>
-  
+}
+</style>
