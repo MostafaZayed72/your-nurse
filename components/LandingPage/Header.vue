4b38581d6937441cb8f7ba1cf850af5d6a4bb30e
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2024-10-08 20:27:08 +0300

    test

--- components/LandingPage/Header.vue
+++ components/LandingPage/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class=" text-center py-4">
       <div class="container mx-auto px-4">
-        <h1 class="text-3xl font-bold">أهلاً بكم <span class="text-cyan-500">Your Nurse</span></h1>
+        <h1 class="text-3xl font-bold">أهلاً بكم في <span class="text-cyan-500">Your Nurse</span></h1>
         <p class="text-lg">اكتشف أفضل العروض والخدمات</p>
       </div>
     </header>
